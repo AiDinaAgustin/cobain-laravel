0235337c1924cff9f1f167655152df92e654c70a
AiDinaAgustin <aidinagustin2@gmail.com>
2023-11-08 18:38:54 +0700

    Revert "belajar factory & faker"
    This reverts commit 8cd53674a1aa3371d485751d99034ea959ef63a3.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,12 +1,10 @@
 <?php
-
 namespace Database\Seeders;
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,24 +12,33 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // User::factory(5)->create();
+        User::factory(3)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
+
+        // // \App\Models\User::factory()->create([
+        // //     'name' => 'Test User',
+        // //     'email' => 'test@example.com',
+        // // ]);
+        // // User::create([
+        // //     'name' => 'Ai Dina Agustin',
+        // //     'email' => 'aidinaagustin@gmail.com',
+        // //     'password' => bcrypt('testuser')
+        // // ]);
+        // // User::create([
+        // //     'name' => 'Dindin',
+        // //     'email' => 'dina@gmail.com',
+        // //     'password' => bcrypt('1234')
+        // // ]);
+
+        // Category::create([
+        //     'name' => 'Web Programming',
+        //     'slug' => 'web-programming'
         // ]);
-
-        User::create([
-            'name' => 'Ai Dina Agustin',
-            'email' => 'aidinaagustin@gmail.com',
-            'password' => bcrypt('testuser')
-        ]);
-        User::create([
-            'name' => 'Dindin',
-            'email' => 'dina@gmail.com',
-            'password' => bcrypt('1234')
-        ]);
-
+        // Category::create([
+        //     'name' => 'Personal',
+        //     'slug' => 'personal'
+        // ]);
         Category::create([
             'name' => 'Web Programming',
             'slug' => 'web-programming'
@@ -41,30 +48,32 @@ class DatabaseSeeder extends Seeder
             'slug' => 'personal'
         ]);
 
-        Post::create([
-            'title' => 'Judul Pertama',
-            'slug' => 'judul-pertama',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
-            'category_id' => 1,
-            'user_id' => 1
-        ]);
-        Post::create([
-            'title' => 'Judul Kedua',
-            'slug' => 'judul-kedua',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
-            'category_id' => 1,
-            'user_id' => 2
-        ]);
-        Post::create([
-            'title' => 'Judul Ketiga',
-            'slug' => 'judul-ketiga',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
-            'category_id' => 2,
-            'user_id' => 1
-        ]);
 
+        Post::factory(5)->create();
+        Post::factory(20)->create();
+        // Post::create([
+        //     'title' => 'Judul Pertama',
+        //     'slug' => 'judul-pertama',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
+        // Post::create([
+        //     'title' => 'Judul Kedua',
+        //     'slug' => 'judul-kedua',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+        //     'category_id' => 1,
+        //     'user_id' => 2
+        // ]);
+        // Post::create([
+        //     'title' => 'Judul Ketiga',
+        //     'slug' => 'judul-ketiga',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+        //     'category_id' => 2,
+        //     'user_id' => 1
+        // ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
-use App\Http\Controllers\PostController;
+use App\Models\Book;
 use App\Models\Post;
+use App\Models\Category;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
-use App\Models\Book;
-use App\Models\Category;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CategoryController;
 
 
 
@@ -44,18 +46,14 @@ Route::get('/books', [BookController::class, 'index']);
 
 Route::get('books/{book:slug}', [BookController::class,'show']);
 
-Route::get('/categories/{category:slug}', function (Category $category) {
-    return view('category', [
-        'title' => $category->name,
-        'posts' => $category->posts,
-        'category' => $category->name,
-    ]);
-}); 
+Route::get('/categories', [CategoryController::class, 'index']);
 
-Route::get('/categories', function () {
-    return view('categories', [
-        'title' => 'Post Categories',
-        'categories' => Category::all()
+Route::get('categories/{category:slug}', [CategoryController::class,'show']);
+
+Route::get('/authors/{user:username}', function (User $user) {
+    return view('posts', [
+        'title' => 'Post by Author',
+        'posts' => $user->posts,
     ]);
 });
 
