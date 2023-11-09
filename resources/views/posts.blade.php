e4910424d68af9a8102fa813e782ef48ecc9a4f1
AiDinaAgustin <aidinagustin2@gmail.com>
2023-11-09 15:03:12 +0700

    postingan terbaru tampil paling atas  fiks

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,11 +6,18 @@
 @if ($posts->count())
     
 <div class="card mb-3">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+    <img src="https://source.unsplash.com/1200x400/?{{ $posts[0]->category->name }}" class="card-img-top" alt="...">
+    <div class="card-body text-center">
+      <h3 class="card-title"><a href="/posts/{{ $posts[0]->slug}}" class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h3> 
+      {{-- untuk mengambil index 0 atau postingan terakhir menggunakan [0]--}}
+    <p>
+        <small class="text-body-secondary">
+        By : <a href=" {{ $posts[0]->user ? '/authors/'.$posts[0]->user->username : '#' }}" class="text-decoration-none">{{ $posts[0]->user->name ?? 'None' }}</a> in <a href="/categories/{{ $posts[0]->category->slug }}" class="text-decoration-none">{{ $posts[0]->category->name }}</a> {{ $posts[0]->created_at->diffForHumans() }}
+        {{-- diffForHumans digunakan untuk menampilkan waktu yang sudah berlalu dari waktu postingan dibuat biar gampang dibaca oleh manusia --}}
+        </small>
+    </p>
+      <p class="card-text">{{ $posts[0]->excerpt }}</p>
+      <p><a href="/posts/{{ $posts[0]->slug}}" class="text-decoration-none btn btn-primary">Read more</a></p>
     </div>
   </div>
    
