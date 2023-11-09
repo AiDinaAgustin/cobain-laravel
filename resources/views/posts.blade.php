1be6f21c6a99f6125c030f7790677e15bf13ab93
AiDinaAgustin <aidinagustin2@gmail.com>
2023-11-09 15:17:54 +0700

    menampilkan post menggunakan card

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,7 +6,7 @@
 @if ($posts->count())
     
 <div class="card mb-3">
-    <img src="https://source.unsplash.com/1200x400/?{{ $posts[0]->category->name }}" class="card-img-top" alt="...">
+    <img src="https://source.unsplash.com/1200x400?{{ $posts[0]->category->name }}" class="card-img-top" alt="{{ $posts[0]->category->name }}">
     <div class="card-body text-center">
       <h3 class="card-title"><a href="/posts/{{ $posts[0]->slug}}" class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h3> 
       {{-- untuk mengambil index 0 atau postingan terakhir menggunakan [0]--}}
@@ -24,8 +24,31 @@
 @else
     <p class="text-center fs-4">No post found.</p>
 @endif
-    
-    @foreach ($posts as $post)
+
+    <div class="container">
+        <div class="row">
+            @foreach ($posts->skip(1) as $post)
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                    <img src="https://source.unsplash.com/500x400?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">
+                    <div class="card-body">
+                      <h5 class="card-title"><a href="/posts/{{ $post->slug}}" class="text-decoration-none">{{ $post->title}}</a></h5>
+                      <p>
+                        <small class="text-body-secondary">
+                        By : <a href=" {{ $post->user ? '/authors/'.$post->user->username : '#' }}" class="text-decoration-none">{{ $post->user->name ?? 'None' }}</a> {{ $post->created_at->diffForHumans() }}
+                        {{-- diffForHumans digunakan untuk menampilkan waktu yang sudah berlalu dari waktu postingan dibuat biar gampang dibaca oleh manusia --}}
+                        </small>
+                    </p>
+                      <p class="card-text">{!! $post->excerpt !!}</p>
+                      <a href="/posts/{{ $post->slug}}" class="btn btn-primary">Read more</a>
+                    </div>
+                  </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
+     
+    @foreach ($posts->skip(1) as $post)
     <article class="mb-5 border-bottom pb-4">
         
         <h2>
