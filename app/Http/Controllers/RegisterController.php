5baa02a94bfd79b33f43e8ad12e1a582cb3734bb
AiDinaAgustin <aidinagustin2@gmail.com>
2023-11-10 22:26:35 +0700

    register-fiks

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class RegisterController extends Controller
 {
@@ -16,14 +17,21 @@ class RegisterController extends Controller
     public function store(Request $request)
     {
        //membuat rule atau peraturan validasi dalam pengisian registtrasi
-       $request->validate([
+       $validatedData = $request->validate([
         'name' => 'required|max:255',
         'username' => 'required|min:3|max:255|unique:users',
         'email'=> 'required|email:dns|unique:users',
         'password'=> 'required|min:5|max:255'
        ]);
 
+       //password akan di hash
+       $validatedData['password'] = bcrypt($validatedData['password']);
+
        //jika validate diatas lolos maka akan dijalankan code dibawah ini
-       dd('registrasi berhasil');
-    }
+       $user = User::create($validatedData);
+
+        // $request->session()->flash('success', 'Registration successfull! Please login');
+
+       return redirect('/login')->with('success', 'Registration successfull! Please login');
+}
 }
