45c6a270d908f3fc9fbb18ba3e231174fd5fa8dc
AiDinaAgustin <aidinagustin2@gmail.com>
2023-11-10 22:45:55 +0700

    login-udah fiks tinggal tambah middlewhere

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -12,4 +12,23 @@ class LoginController extends Controller
             "title" => "Login",
             "active" => "login"]);
     }
+
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email:dns'],
+            'password' => ['required'],
+        ]);
+
+        //jika validasi gagal maka akan kembali ke halaman login dan menampilkan error
+        //attempt digunakan untuk login
+        //session()->regenerate() digunakan untuk menghapus session yang lama dan membuat session yang baru
+        //intended digunakan untuk mengarahkan ke halaman sebelumnya
+        if (auth()->attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended('/dashboard');
+        }
+
+        return back()->with('loginError', 'Login failed!');
+    }
 }
