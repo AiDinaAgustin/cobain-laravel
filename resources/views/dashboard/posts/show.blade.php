d496c0acbcb05c746a85e13478932384712316e0
AiDinaAgustin <aidinagustin2@gmail.com>
2023-11-12 09:54:18 +0700

    delete-fiks

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -7,7 +7,11 @@
                 <h1 class="mb-3">{{ $post->title}}</h1>
                 <a href="" class="btn btn-success"><i class="bi bi-arrow-left"></i>Back to all my posts</a>
                 <a href="" class="btn btn-warning"><i class="bi bi-pencil-square"></i>Edit</a>
-                <a href="" class="btn btn-danger"><i class="bi bi-trash-fill"></i>Delete</a>
+                <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+                    @csrf
+                    @method('delete')
+                    <button class="btn btn-danger border-0" onclick="return confirm('Are you sure?')"><i class="bi bi-trash-fill"></i>Delete</button>
+                  </form>
                 <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" class="img-fluid mt-3">
                 
                 <article class="my-3 fs-6">
